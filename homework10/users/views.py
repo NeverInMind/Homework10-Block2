a41c3c82e4dd090d5fc959d3764b4e46a40e0035
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-11-05 22:45:31 +0200

    made edit

--- homework10/users/views.py
+++ homework10/users/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+from .forms import RegisterForm
+
+
 def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(data=request.POST)
+    if request.user.is_authenticated:
+        return redirect(to='quotes:index')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            new_user = form.save(commit=True)
-            login(request, new_user)
-            return redirect("quotes:index")
-    else:
-        form = UserCreationForm()
-    context = {"form": form}
-    return render(request, "registration/register.html", context)
+            form.save()
+            return redirect(to='quotes:index')
+        else:
+            return render(request, 'registration/register.html', context={"form": form})
+
+    return render(request, 'registration/register.html', context={"form": RegisterForm()})
